5538f0652e3763fe193fb916875033c65f427728
Maxh <44454544+1-max-1@users.noreply.github.com>
2023-06-24 12:43:55 +1200

    Fixed json usb device list not deserializing

--- WASMSerialTerminal/Models/USBDeviceStructures.cs
+++ WASMSerialTerminal/Models/USBDeviceStructures.cs
@@ -1,37 +1,18 @@
-﻿using System.Collections.Immutable;
-
-namespace WASMSerialTerminal {
+﻿namespace WASMSerialTerminal {
 	internal class USBDevice {
-		public string ID { get; private set; }
-		public string Name { get; private set; }
-
-		public USBDevice(string id, string name) {
-			ID = id;
-			Name = name;
-		}
+		public required string ID { get; set; }
+		public required string Name { get; set; }
 	}
 
 	internal class USBVendor {
-		public string ID { get; private set; }
-		public string Name { get; private set; }
-		public ImmutableArray<USBDevice> Devices { get; private set; }
-
-		public USBVendor(string iD, string name, ImmutableArray<USBDevice> devices) {
-			ID = iD;
-			Name = name;
-			Devices = devices;
-		}
+		public required string ID { get; set; }
+		public required string Name { get; set; }
+		public required List<USBDevice> Devices { get; set; }
 	}
 
 	internal class USBVendorList {
-		public ImmutableArray<USBVendor> Vendors { get; private set; }
-		public string Version { get; private set; }
-		public string Date { get; private set; }
-
-		public USBVendorList(ImmutableArray<USBVendor> vendors, string version, string date) {
-			Vendors = vendors;
-			Version = version;
-			Date = date;
-		}
+		public required List<USBVendor> Vendors { get; set; }
+		public required string Version { get; set; }
+		public required string Date { get; set; }
 	}
 }
